56317e7693774aadbb9f65f36f8817709aeb722e
ASW-Analyst <andrew.wilson46@nhs.net>
2022-09-26 18:02:18 +0000

    RMarkdown creation

--- Mapping_Leaflet_LSOA.R
+++ Mapping_Leaflet_LSOA.R
@@ -47,7 +47,7 @@ lsoa_data_nw<-subset(lsoa_data, Region == "North West")
 
 # Create Palettes --------------------------------------------------------
 
-pal_fac<-colorFactor(palette = "Purples", levels = 10:1, domain = lsoa_data_nw$`Index.of.Multiple.Deprivation.(IMD).Decile`)
+pal_fac<-colorFactor(palette = "Blues", levels = 10:1, domain = lsoa_data_nw$`Index.of.Multiple.Deprivation.(IMD).Decile`)
 
 # Create map --------------------------------------------------------------
 
@@ -55,9 +55,52 @@ lat<-53.8
 lng<--2.5
 
 leaflet(lsoa_data_nw)%>%
-  addPolygons(stroke = TRUE, weight = 1, opacity = 1, fillOpacity = 0.6,
+  addPolygons(stroke = TRUE, weight = 0.4, opacity = 1, fillOpacity = 0.6,
               color = "black", fillColor = ~pal_fac(`Index.of.Multiple.Deprivation.(IMD).Decile`),
               label = paste0(lsoa_data_nw$LSOA11NM,": Decile - ", lsoa_data_nw$`Index.of.Multiple.Deprivation.(IMD).Decile`))%>%
   addProviderTiles(providers$CartoDB.Voyager)%>%
-  addLegend(values = lsoa_data_nw$`Index.of.Multiple.Deprivation.(IMD).Decile`, pal = pal_fac, title = "Attainment Score",position = "topright")%>%
+  addLegend(values = lsoa_data_nw$`Index.of.Multiple.Deprivation.(IMD).Decile`, pal = pal_fac, title = "IMD Decile - 2019",position = "topright")%>%
   setView(lng, lat, zoom = 8)
+
+
+# Create C&M Map ----------------------------------------------------------
+
+lat<-53.3
+lng<--2.5
+
+lsoa_data_cm<-subset(lsoa_data_nw, System == "Cheshire & Merseyside")
+
+leaflet(lsoa_data_cm)%>%
+  addPolygons(stroke = TRUE, weight = 0.4, opacity = 1, fillOpacity = 0.6,
+              color = "black", fillColor = ~pal_fac(`Index.of.Multiple.Deprivation.(IMD).Decile`),
+              label = paste0(lsoa_data_cm$LSOA11NM,": Decile - ", lsoa_data_cm$`Index.of.Multiple.Deprivation.(IMD).Decile`))%>%
+  addProviderTiles(providers$CartoDB.Voyager)%>%
+  addLegend(values = lsoa_data_cm$`Index.of.Multiple.Deprivation.(IMD).Decile`, pal = pal_fac, title = "IMD Decile - 2019",position = "topright")%>%
+  setView(lng, lat, zoom = 9)
+
+
+
+# Create C&M Map with LA outline ------------------------------------------
+
+
+
+lads <- geojsonio::geojson_read(
+  x = "regions.geojson",  # the same file_path as above
+  what = "sp"  # returns the read with spatial class
+)
+
+##Filter shapes for North West only
+NW_LTLA<-read_excel("NW_LTLA_Mapping.xlsx")
+lads_nw<-merge(lads,NW_LTLA, by.x = "lad17cd", "areaCode")
+lads_nw<-subset(lads_nw, Region == "North West")
+lads_cm<-subset(lads_nw, System == "Cheshire & Merseyside")
+
+leaflet(lsoa_data_cm)%>%
+  addPolygons(data = lads_cm, color = "black", weight = 2)%>%
+  addPolygons(stroke = TRUE, weight = 0.4, opacity = 1, fillOpacity = 0.6,
+              color = "grey", fillColor = ~pal_fac(`Index.of.Multiple.Deprivation.(IMD).Decile`),
+              label = paste0(lsoa_data_cm$LSOA11NM,": Decile - ", lsoa_data_cm$`Index.of.Multiple.Deprivation.(IMD).Decile`))%>%
+  addProviderTiles(providers$CartoDB.Voyager)%>%
+  addLegend(values = lsoa_data_cm$`Index.of.Multiple.Deprivation.(IMD).Decile`, pal = pal_fac, title = "IMD Decile - 2019",position = "topright")%>%
+  setView(lng, lat, zoom = 9)
+
